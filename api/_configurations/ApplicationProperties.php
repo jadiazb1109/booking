970904ddee0f309eb49f74787be9153daa1b7176
Jorge Diaz <ing.jorgediaz@outlook.com>
2025-06-05 08:30:53 -0500

    se configura las variables de entorno

--- api/_configurations/ApplicationProperties.php
+++ api/_configurations/ApplicationProperties.php
@@ -1,12 +1,12 @@
 <?php
 
-define('__DB_HOST__', '127.0.0.1');
+define('__DB_HOST__', 'localhost;port=3306');
 
-define('__DB_USERNAME__', 'root');
+define('__DB_USERNAME__', 'u480480233_booking_root');
 
-define('__DB_PASSWORD__', '3sc0rp10n');
+define('__DB_PASSWORD__', '3sc0rp10n.JD');
 
-define('__DB_NAME__', 'booking');;
+define('__DB_NAME__', 'u480480233_booking');;
 
 define('__DB_CHARSET__', 'utf8');
 
